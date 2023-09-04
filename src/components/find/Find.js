68adc39a8225e040de2ee086c6d94a05acf00c7f
amita <amitasher4@gmail.com>
2023-09-04 12:50:46 +0300

    update project

--- src/components/find/Find.js
+++ src/components/find/Find.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../find/FindStyle.css";
+import "../find/findStyle.css";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar } from "swiper/modules";
