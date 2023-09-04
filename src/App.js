5322841bbef7b2f305d7c8f89d7ef8a2b822470c
amita <amitasher4@gmail.com>
2023-09-04 13:18:25 +0300

    fix project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
-import Find from "./components/find/Find";
+
 import Driver from "./components/driver/Driver";
 import Luxury from "./components/luxury/Luxury";
 
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
-      <Find />
+
       <Driver />
       <Luxury />
     </div>
